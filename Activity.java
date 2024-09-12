a5080c1dc1a8a7b3c36bf6f4c4ccf7be6138baa9
chaweee <23-09852@g.batstate-u.edu.ph>
2024-09-12 15:04:50 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,14 @@
+class Activity{
+    public static void main(String[] args) {
+        
+        Dog objDog = new Dog();
+        Dog numberofPuppies = Puppies();
+
+        objDog.name = "Tata";
+        objDog.name = "Tate";    
+        objDog.name = "Tati";    
+        objDog.name = "Tato";    
+        objDog.name = "Tatu";    
+
+    
+}
---
+++ Dog.java
@@ -0,0 +1,18 @@
+class Dog{
+    String name;
+    boolean isMale;
+    int numberofPuppies;
+Dog[] Puppies = new Dog[5];
+
+public String[] showPuppies(){
+    return showPuppies();
+
+
+}
+
+
+    	
+
+
+
+}
