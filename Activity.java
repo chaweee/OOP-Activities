f26ce02e7f5dd6163e61360ddd1c61b89dc00e5d
chaweee <23-09852@g.batstate-u.edu.ph>
2024-09-19 22:16:59 +0800

    Activity.java

--- Activity.java
+++ Activity.java
@@ -4,8 +4,17 @@ class Activity{
         aso.name = "toto";
         aso.puppies = 5;
 
-        aso.
-     
+        aso.doggy[0]= "tata";
+        aso.doggy[1]= "tete";
+        aso.doggy[2]= "tutu";
+        aso.doggy[3]= "tuti";
 
-    
-}
+        System.out.println(aso.showName());
+        System.out.println(aso.numofPuppies());
+        System.out.println(x:"Woof! Woof!);
+
+        for (int i = -; 1 <5; i++) {
+            System.out.println(aso.doggy[i]);
+        }
+    }
+    }
