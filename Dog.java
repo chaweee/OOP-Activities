67efae6761ff5ee26a638b2444be11af811e678f
chaweee <23-09852@g.batstate-u.edu.ph>
2024-09-19 17:23:27 +0800

    Dog.java

--- Dog.java
+++ Dog.java
@@ -1,18 +1,25 @@
-class Dog{
-    String name;
-    boolean isMale;
-    int numberofPuppies;
-Dog[] Puppies = new Dog[5];
+public class Dog {
+        String name;
+        boolean ismale;
+        int numofPuppies;
+        Dog[] Puppies = new Dog[5];
+        Dog Name;
+    
+    public void SetName(String Name){
+            
+        }
+        
+    public String showName(){
+            return name;
+        }
+    public void showNumberOfPuppies(){
+   
+        }
 
-public String[] showPuppies(){
-    return showPuppies();
+ public int setNumOfPuppies(int numofPuppies){
+            return numofPuppies;
 
-
-}
-
-
-    	
-
-
+        }
+    
 
 }
